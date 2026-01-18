3b0f4a4ce166c570b6e1b3efdef274b602bbbcd8
Kekus_Gigabyte <user@WIN-G3Q8CVB0RMM>
2026-01-18 15:13:21 +0100

    Update

--- FilmLibrary/Controllers/FilmController.cs
+++ FilmLibrary/Controllers/FilmController.cs
@@ -1,12 +1,77 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using FilmLibrary.Data;
+using FilmLibrary.Models;
 namespace FilmLibrary.Controllers;
 
 public class FilmController : Controller
 {
+    
     // GET
     public IActionResult Index()
     {
+        return View(DataStorage.Films);
+    }
+    
+    //GET
+    public IActionResult Description(int id)
+    {
+        Film film = DataStorage.Films.First(f => f.Id == id);
+        return View(film);
+    }
+    
+    
+    //CRUD - operations (create-update-delete)
+    
+    //GET
+    public IActionResult Create()
+    {
         return View();
+    }
+    
+    //POST
+    [HttpPost]
+    public IActionResult Create(Film film)
+    {
+        film.Id = DataStorage.Films.Count + 1;
+        DataStorage.Films.Add(film);
+
+        return View("Index");
+    }
+    
+    //GET
+    public IActionResult Edit(int id)
+    {
+        Film film = DataStorage.Films.First(f => f.Id == id);
+        return View(film);
+    }
+    
+    //POST
+    /*
+    public IActionResult Edit(Film film)
+    {
+        var removedFilm = DataStorage.Films.First(f => f.Id == film.Id);
+
+        DataStorage.Films.Remove(removedFilm);
+        DataStorage.Films.Add(film);
+
+        return View("Index");
+    }
+    */
+    
+    //GET
+    public IActionResult Delete(int id)
+    {
+        Film film = DataStorage.Films.First(f => f.Id == id);
+        return View(film);
+    }
+    
+    //POST
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var removedFilm = DataStorage.Films.First(f => f.Id == id);
+        DataStorage.Films.Remove(removedFilm);
+        
+        return View("Index");
     }
 }
--- FilmLibrary/Data/DataStorage.cs
+++ FilmLibrary/Data/DataStorage.cs
@@ -7,9 +7,9 @@ public static class DataStorage
 {
     public static List<Film> Films = new()
     {
-        new Film() {Id = 1, ImagePath = "D:\\RProjects\\FilmLibrary\\FilmLibrary\\wwwroot\\Images\\Diego DMC.png", Title = "Reznya Chertey", AgeRestriction = "18+", Plot = "Plot", Description = "Description"},
-        new Film() {Id = 2,ImagePath = "D:\\RProjects\\FilmLibrary\\FilmLibrary\\wwwroot\\Images\\DOOM DIEGO.png", Title = "DoomM the Peak ages", AgeRestriction = "18+", Plot = "Plot", Description = "Descroption"},
-        new Film() {Id = 3, ImagePath = "D:\\RProjects\\FilmLibrary\\FilmLibrary\\wwwroot\\Images\\gerfds.png", Title = "Novenkiy", AgeRestriction = "18+", Plot = "Plot", Description = "Description"},
-        new Film() {Id = 4, ImagePath = "D:\\RProjects\\FilmLibrary\\FilmLibrary\\wwwroot\\Images\\On yak.png", Title = "On yak", AgeRestriction = "13+", Plot = "Plot", Description = "Description"}
+        new Film() {Id = 1, ImagePath = "\\Images\\Diego DMC.png", Title = "Reznya Chertey", AgeRestriction = "18+", Plot = "Plot", Description = "Description"},
+        new Film() {Id = 2,ImagePath = "\\Images\\DOOM DIEGO.png", Title = "DoomM the Peak ages", AgeRestriction = "18+", Plot = "Plot", Description = "Descroption"},
+        new Film() {Id = 3, ImagePath = "\\Images\\gerfds.png", Title = "Novenkiy", AgeRestriction = "18+", Plot = "Plot", Description = "Description"},
+        new Film() {Id = 4, ImagePath = "\\Images\\On yak.png", Title = "On yak", AgeRestriction = "13+", Plot = "Plot", Description = "Description"}
     };
 }
